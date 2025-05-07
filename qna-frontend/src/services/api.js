53a069239626c8cf45a941aed92fc87b6d1e4ab7
suwini244 <it22158840@my.sliit.lk>
2025-05-07 19:39:52 +0530

    Delete qna-frontend directory

--- qna-frontend/src/services/api.js
+++
@@ -1,8 +0,0 @@
-// src/services/api.js
-import axios from 'axios';
-
-const API = axios.create({
-  baseURL: '', // Leave this empty if using proxy
-});
-
-export default API;
--- qna-frontend/src/services/authService.js
+++
@@ -1,5 +0,0 @@
-// src/services/authService.js
-import API from './api';
-
-export const login = (data) => API.post('/auth/login', data);
-export const register = (data) => API.post('/auth/register', data);
